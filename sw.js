697020cc243fe30c26499c3d45e2edbd47da706e
Kaosu304prachinburi <yusubahato@gmail.com>
2026-03-20 13:12:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-const CACHE_NAME = "kaosu-store-v1";
+const CACHE_NAME = "kaosu-app-v1";
 const FILES_TO_CACHE = [
   "./index.html",
   "./store.html",
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
